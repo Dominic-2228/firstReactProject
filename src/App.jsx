a645275bc0405899326bac9ad8d6de188789a0aa
Dominic Tillison <22tilldj@gmail.com>
2025-04-16 11:53:22 -0400

    counter Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react"
+
 export const App = () => {
-  return <div className="welcome">Welcome to your first React Application!</div>
+
+  const [count, setCount] = useState(0) //[stateVariable, setterFunction]
+
+  const handleButtonClick = () => {
+    setCount(count + 1)
 }
+
+  return <div>
+    <div className="welcome">Welcome to your first React Application!</div>
+    <p>This is a test</p>
+    <button className="btn-secondary"onClick={handleButtonClick}>Click Me!</button>
+    <div>Count: {count}</div>
+  </div>
+}
+
